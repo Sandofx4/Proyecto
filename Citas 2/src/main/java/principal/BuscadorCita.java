7fb1a773c0ae2173f6fcc18da7139fe49c5b5b86
Sandofx4 <fio.sandoval.vega@gmail.com>
2025-07-26 18:30:21 -0600

    Add files via upload

---
+++ Citas 2/src/main/java/principal/BuscadorCita.java
@@ -0,0 +1,21 @@
+package principal;
+
+import java.util.ArrayList;
+
+public class BuscadorCita {
+
+    public static void buscarCitaPorCedula(ArrayList<Cita> agenda, int cedula) {
+        boolean encontrada = false;
+        for (Cita cita : agenda) {
+            if (cita.getId() == cedula) {
+                System.out.println("Cita encontrada: " + cita);
+                encontrada = true;
+                break;
+            }
+        }
+
+        if (!encontrada) {
+            System.out.println("No se encontró ninguna cita con ese número de cédula.");
+        }
+    }
+}
